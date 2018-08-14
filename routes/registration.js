94a0f83479f95b9e1945a96b5f073aecaab995e8
Denis Lutov <lutov.denis@gmail.com>
2018-08-14 22:46:21 +0200

    User Registration

--- routes/registration.js
+++ routes/registration.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'I am bandwagon' });
+    res.render('registration', { title: 'your favourite website' });
 });
 
 module.exports = router;
